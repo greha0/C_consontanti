af59dfea0eb6b63aa6c36523d0f21492c58b4b3d
greta <greta.maria.brugnatti.2007@calvino.edu.it>
2024-04-12 11:55:17 +0200

    finito

--- main.c
+++ main.c
@@ -7,53 +7,84 @@
 #include <stdio.h>
 #define DIM 20
 
-//dichiarazione delle funzioni
+//dichiarazioni delle funzioni
 int isConson(char c);
-void prendiConsonante(char v[], char cons[]);
-char esimaCons(char v[]);
+char esimaCons(char v[], int pos);
 void eliminaACapo(char str[]);
 char toLower(char c);
 
 int main (void){
 
-    //dichiarazione delle variabili
-    char str[DIM]; //stringa inserita dall'utente
-    int consonanti;
+    //dichiarazioni delle variabili
+    char str[DIM];
+    char conson[DIM];
+    char cons;
+    conson[0]='\0';
+    int consonante;
+    int cont=0;
+    int ennesima;
 
-    //richiesta dei dati all'utente
+  //
     printf("Inserire il testo: ");
     fflush(stdout);
 
+
     fgets(str, DIM, stdin);
 
-    eliminaACapo(str); //elimina il \n alla fine
+    eliminaACapo(str);
 
-    //trasformo tutte le maiuscole in minuscole
+
     for(int i=0; str[i]!='\0'; i++){
         str[i]=toLower(str[i]);
     }
 
     for(int i=0; str[i]!='\0'; i++){
-        consonanti=isConson(str[i]);
-        printf("%d\n", consonanti);
+        consonante=isConson(str[i]);
+        if(consonante==1){
+
+            conson[cont]=str[i];
+            conson[cont+1]='\0';
+
+            cont++;
+        }
+
+        if(consonante != 2) {
+            printf("%d\n", consonante);
+            fflush(stdout);
+        }
     }
 
+    printf("%s\n", conson);
+    fflush(stdout);
+
+    printf("Quale ennesima consonante vuoi? (da 1 a ...)");
+    fflush(stdout);
+    scanf("%d", &ennesima);
+    fflush(stdin);
+
+    cons = esimaCons(conson, ennesima);
+    printf("%c", cons);
 
 
 }
+//funzione che controlla lettere
 int isConson(char c){
     if(c =='a' || c =='e' || c =='i' || c=='o' ||  c == 'u'){
-        return 0; // non è una consonante
+        return 0;
     } else {
-        return 1; // è una consonante
+        if(c == ' ' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' ){
+            return 2;
+        } else {
+            return 1;
+        }
     }
 }
 
-void prendiConsonante(char v[], char cons[]){
-
+char esimaCons(char v[], int pos){
+    return v[pos-1];
 }
-char esimaCons(char v[]);
 
+//Trasforma maiuscule in minuscole
 char toLower(char c) {
     if (c >= 'A' && c <= 'Z') {
         c = c - 'A' + 'a';
@@ -61,6 +92,7 @@ char toLower(char c) {
     return c;
 }
 
+//
 void eliminaACapo(char str[]) {
 
     int i;
